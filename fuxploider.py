c863c98116cf9caece8bc99718a1521240c2d80d
almandin <vjarry@sigma.fr>
2017-08-07 17:18:29 +0200

    version number changed

--- fuxploider.py
+++ fuxploider.py
@@ -3,7 +3,7 @@ import re,requests,argparse,logging,os,coloredlogs,datetime,getpass,tempfile
 from utils import *
 from UploadForm import UploadForm
 
-version = "0.1.9"
+version = "0.2.0"
 logging.basicConfig(datefmt='[%m/%d/%Y-%H:%M:%S]')
 logger = logging.getLogger("fuxploider")
 
